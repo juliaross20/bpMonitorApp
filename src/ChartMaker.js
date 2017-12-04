7e2e7d6e93e88182bb930d949dabc32f03ed3548
Julia Ross <jlr72@duke.edu>
2017-12-04 00:29:34 -0500

    just made it a little nicer

--- src/ChartMaker.js
+++ src/ChartMaker.js
@@ -1,6 +1,6 @@
 import React, { Component} from 'react';
 import ReactDom from 'react-dom';
-import { LineChart, Line, XAxis, YAxis } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 import 'particle-api-js'
 
 class ChartMaker extends Component{
@@ -52,6 +52,8 @@ class ChartMaker extends Component{
                         margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
 			<XAxis dataKey="name" />
  				 <YAxis />
+			<Tooltip />
+ 			<Legend />
 			<Line type="monotone" dataKey="syst" stroke="#8884d8" />
 			<Line type="monotone" dataKey="diast" stroke="#82ca9d" />
                         </LineChart>
